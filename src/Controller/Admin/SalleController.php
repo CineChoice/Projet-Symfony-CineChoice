b504868850a764eb2c039f8e5695055c50797bde
unknown <victoria.carre78@gmail.com>
2023-11-13 14:25:20 +0100

    gestion admin salle (sans les modals)

--- src/Controller/Admin/SalleController.php
+++ src/Controller/Admin/SalleController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Room;
+use App\Form\SalleType;
 use App\Repository\RoomRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,4 +41,55 @@ class SalleController extends AbstractController
             'lesSalles' => $salles
         ]);
     }
+
+    #[Route('/admin/salle/supr/{id}', name: 'admin_salle_supr', methods: ['GET'])]
+    public function suprSalle(Room $salle, EntityManagerInterface $manager): Response
+    {
+        $nbSeance = $salle->getSessions()->count();
+
+        if ($nbSeance > 0) {
+            $this->addFlash("danger", "Vous ne pouvez pas supprimer cette salle car $nbSeance Séance(s) y sont associés!");
+        } else {
+            $manager->remove($salle);
+            $manager->flush();
+
+            $this->addFlash("success", "La salle a bien été supprimé!");
+        }
+
+        return $this->redirectToRoute('admin_salles');
+    }
+
+
+        #[Route('/admin/salle/ajout', name: 'admin_salle_ajout', methods: ['GET', 'POST'])]
+        #[Route('/admin/salle/modif/{id}', name: 'admin_salle_modif', methods: ['GET', 'POST'])]
+        public function ajoutModifSalle(Room $salle = null, Request $request, EntityManagerInterface $manager): Response
+        {
+            if (!$salle) {
+                $salle = new Room(); // Initialisez une nouvelle instance de Film si $film est null
+                $mode = "ajouté";
+            } else {
+                $mode = "modifié";
+            }
+        
+            $form = $this->createForm(SalleType::class, $salle); // provient du formulaire
+        
+            $form->handleRequest($request);
+        
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $manager->persist($salle);
+                $manager->flush();
+        
+                $this->addFlash("success", "La salle a bien été $mode!");
+        
+                return $this->redirectToRoute('admin_salles');
+            }
+        
+            return $this->render('admin/salle/formAjoutModifSalle.html.twig', [
+                'formSalle' => $form->createView(),
+            ]);
+        }
+
+
+
 }

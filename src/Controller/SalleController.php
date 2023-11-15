417c36ec552e6956d62703809b4eee992d50219e
unknown <victoria.carre78@gmail.com>
2023-11-15 20:11:41 +0100

    fiche salle

--- src/Controller/SalleController.php
+++ src/Controller/SalleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Twig\Environment;
 use App\Repository\RoomRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SalleController extends AbstractController
 {
+    private $twig;
+
+        public function __construct(Environment $twig)
+        {
+            $this->twig = $twig;
+        }
+
     #[Route('/salles', name: 'salles', methods:['GET'])]
 
     public function listeSalles (RoomRepository $repo, PaginatorInterface $paginator, Request $request) : Response
@@ -25,5 +33,14 @@ class SalleController extends AbstractController
         return $this->render('salle/listeSalles.html.twig', [
             'lesSalles' => $salles
         ]);
+    }
+
+    #[Route('/salles/{id}', name: 'salle_fiche', methods:['GET'])]
+    public function ficheSalle(int $id, RoomRepository $repo): Response
+    {
+        $salle = $repo->find($id);
+        return new Response($this->twig->render('salle/ficheSalle.html.twig', [
+            'laSalle' => $salle,
+        ]));
     }
 }
